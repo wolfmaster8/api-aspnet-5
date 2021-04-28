a4c05654ce20d388b8606ba0510744e8faa2dbb4
wolfmaster8 <jflobom@gmail.com>
2021-04-28 12:49:32 -0500

    Add calculator & all operations

---
+++ RestAPI/RestAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("substract/{firstNumber}/{secondNumber}")]
+        public IActionResult Substract(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var substract = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(substract.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiplication.ToString());
+            }
+            return BadRequest("Invalid Inputs");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var decimalSecondNumber = ConvertToDecimal(secondNumber);
+                if(CanDivide(decimalSecondNumber))
+                {
+                    var division = ConvertToDecimal(firstNumber) / decimalSecondNumber;
+                    return Ok(division.ToString());
+                }
+                return BadRequest("Can't divide by zero");
+            }
+            return BadRequest("Invalid Inputs");
+
+        }
+
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult Average(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var numberSum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                var average = numberSum / 2;
+                return Ok(average.ToString());
+            }
+            return BadRequest("Invalid Inputs");
+
+        }
+
+        [HttpGet("square/{stringNumber}")]
+        public IActionResult Square(string stringNumber)
+        {
+            if (IsNumeric(stringNumber))
+            {
+                var square = Math.Sqrt((double)ConvertToDecimal(stringNumber)) ;
+                return Ok(square.ToString());
+            }
+            return BadRequest("Invalid Inputs");
+
+        }
+
+        private bool CanDivide(decimal secondNumber)
+        {
+            if(secondNumber != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private decimal ConvertToDecimal(string stringNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(stringNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+   
+
+        private bool IsNumeric(string stringNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                stringNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+
+            return isNumber;
+        }
+    }
+}
--- RestAPI/RestAPI/Startup.cs
+++ RestAPI/RestAPI/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,9 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace api
+namespace RestApi
 {
     public class Startup
     {
@@ -26,12 +25,7 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "api", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +34,8 @@ namespace api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
+          
 
             app.UseHttpsRedirection();
 
